ef1e7b67a277534d1155d7665817ba4fb6e2383d
Alaa-Abu-Elkhier <37739113+Alaa-Abu-Elkhier@users.noreply.github.com>
2022-12-19 21:08:33 +0200

    edit app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,92 @@
+import React, { useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
-import { ConfigProvider, Button, theme } from 'antd';
+import footerbg from './assets/footerbg.png';
+import { ConfigProvider, Button, theme, Layout } from 'antd';
+
+import { Routes, Route, Outlet, Link } from 'react-router-dom';
+import AboutUs from './pages/AboutUsPage/AboutUs';
 import HomePage from './pages/HomePage/HomePage';
+import AppHeader from './components/AppHeader';
+import AppFooter from './components/AppFooter';
+import ContactUs from './pages/ContactUsPage/ContactUs';
+const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
+  const [isDark, setisDark] = useState(true);
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  const switchTheme = (isDarkMode) => {
+    setIsDarkMode((prev) => !prev);
+    console.log(isDarkMode);
+  };
+
   return (
     <div className='App'>
-      <ConfigProvider
-        theme={{
-          algorithm: theme.defaultAlgorithm,
-          token: {
-            // colorPrimary: '#00b96b',
-            // colorBgMask: '#000',
-            // fontSize: 24,
-          },
-        }}>
-        <HomePage />;
-      </ConfigProvider>
+      {isDarkMode ? (
+        <ConfigProvider
+          theme={{
+            algorithm: theme.defaultAlgorithm,
+            token: {
+              // colorPrimary: '#00b96b',
+              // colorBgMask: '#000',
+              // fontSize: 24,
+            },
+          }}>
+          <Layout>
+            <Header>
+              <AppHeader switchTheme={switchTheme} isDarkMode={isDarkMode} />
+            </Header>
+            <Content>
+              <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/about-us' element={<AboutUs />} />
+                <Route path='/contact-us' element={<ContactUs />} />
+              </Routes>
+            </Content>
+            <Footer
+              style={{
+                textAlign: 'center',
+                backgroundColor: 'black',
+                backgroundImage: `url(${footerbg})`,
+              }}>
+              <AppFooter />
+            </Footer>
+          </Layout>
+        </ConfigProvider>
+      ) : (
+        <ConfigProvider
+          theme={{
+            algorithm: theme.darkAlgorithm,
+            token: {
+              // colorPrimary: '#00b96b',
+              // colorBgMask: '#000',
+              // fontSize: 24,
+            },
+          }}>
+          <Layout>
+            <Header>
+              <AppHeader />
+            </Header>
+            <Content>
+              <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/about-us' element={<AboutUs />} />
+                <Route path='/contact-us' element={<ContactUs />} />
+              </Routes>
+            </Content>
+            <Footer
+              style={{
+                textAlign: 'center',
+                backgroundColor: 'black',
+                backgroundImage: `url(${footerbg})`,
+              }}>
+              <AppFooter />
+            </Footer>
+          </Layout>
+        </ConfigProvider>
+      )}
     </div>
   );
 }

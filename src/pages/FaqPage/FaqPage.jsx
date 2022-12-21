aca0ebb793c16094778643c914b6b8343e136193
Alaa-Abu-Elkhier <37739113+Alaa-Abu-Elkhier@users.noreply.github.com>
2022-12-21 04:14:11 +0200

    add responsive design

--- src/pages/FaqPage/FaqPage.jsx
+++ src/pages/FaqPage/FaqPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Row, Col, Typography, Select, Collapse } from 'antd';
+import { useMediaQuery } from 'react-responsive';
 
 import faqBg from '../../assets/faqBg.png';
 // import aboutUs1 from '../../assets/aboutUs1.png';
@@ -12,47 +13,101 @@ Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
 `;
 
 function FaqPage() {
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' });
   return (
-    <div>
-      <Row style={{ marginTop: '100px' }}>
-        <Col span={10} style={{ marginTop: '120px', marginLeft: '30px' }}>
-          <Row justify={'start'}>
-            {' '}
+    <>
+      {isTabletOrMobile ? (
+        <div>
+          <Row justify={'center'} style={{ marginTop: '100px' }}>
+            <img src={faqBg} alt='faqBg' style={{ width: '300px' }} />
+          </Row>
+          <Row
+            justify={'center'}
+            style={{
+              marginTop: '30px',
+            }}>
             <Typography.Title
-              level={1}
+              level={2}
               style={{
                 margin: 0,
                 marginBottom: '30px',
               }}>
               FAQ
             </Typography.Title>
-          </Row>{' '}
-          <Collapse>
-            <Panel
-              style={{ backgroundColor: 'white' }}
-              header='What is INVASSO ?'
-              key='1'>
-              <p style={{ color: '#5A7184', textAlign: 'left' }}>{text}</p>
-            </Panel>
-            <Panel
-              style={{ backgroundColor: 'white' }}
-              header='What is INVASSO ?'
-              key='2'>
-              <p style={{ color: '#5A7184', textAlign: 'left' }}>{text}</p>
-            </Panel>
-            <Panel
-              style={{ backgroundColor: 'white' }}
-              header='What is INVASSO ?'
-              key='3'>
-              <p style={{ color: '#5A7184', textAlign: 'left' }}>{text}</p>
-            </Panel>
-          </Collapse>
-        </Col>
-        <Col span={12}>
-          <img src={faqBg} alt='faqBg' />
-        </Col>
-      </Row>
-    </div>
+          </Row>
+          <Row
+            justify={'center'}
+            style={{
+              marginBottom: '30px',
+            }}>
+            <Col span={20}>
+              {' '}
+              <Collapse>
+                <Panel
+                  style={{ backgroundColor: 'white' }}
+                  header='What is INVASSO ?'
+                  key='1'>
+                  <p style={{ color: '#5A7184', textAlign: 'left' }}>{text}</p>
+                </Panel>
+                <Panel
+                  style={{ backgroundColor: 'white' }}
+                  header='What is INVASSO ?'
+                  key='2'>
+                  <p style={{ color: '#5A7184', textAlign: 'left' }}>{text}</p>
+                </Panel>
+                <Panel
+                  style={{ backgroundColor: 'white' }}
+                  header='What is INVASSO ?'
+                  key='3'>
+                  <p style={{ color: '#5A7184', textAlign: 'left' }}>{text}</p>
+                </Panel>
+              </Collapse>
+            </Col>{' '}
+          </Row>
+        </div>
+      ) : (
+        <div>
+          <Row style={{ marginTop: '100px' }}>
+            <Col span={10} style={{ marginTop: '120px', marginLeft: '30px' }}>
+              <Row justify={'start'}>
+                {' '}
+                <Typography.Title
+                  level={1}
+                  style={{
+                    margin: 0,
+                    marginBottom: '30px',
+                  }}>
+                  FAQ
+                </Typography.Title>
+              </Row>{' '}
+              <Collapse>
+                <Panel
+                  style={{ backgroundColor: 'white' }}
+                  header='What is INVASSO ?'
+                  key='1'>
+                  <p style={{ color: '#5A7184', textAlign: 'left' }}>{text}</p>
+                </Panel>
+                <Panel
+                  style={{ backgroundColor: 'white' }}
+                  header='What is INVASSO ?'
+                  key='2'>
+                  <p style={{ color: '#5A7184', textAlign: 'left' }}>{text}</p>
+                </Panel>
+                <Panel
+                  style={{ backgroundColor: 'white' }}
+                  header='What is INVASSO ?'
+                  key='3'>
+                  <p style={{ color: '#5A7184', textAlign: 'left' }}>{text}</p>
+                </Panel>
+              </Collapse>
+            </Col>
+            <Col span={12}>
+              <img src={faqBg} alt='faqBg' />
+            </Col>
+          </Row>
+        </div>
+      )}
+    </>
   );
 }
 

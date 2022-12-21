e294dab7537b27e957ea3d91fdc2fd166fd84467
Alaa-Abu-Elkhier <37739113+Alaa-Abu-Elkhier@users.noreply.github.com>
2022-12-21 03:05:38 +0200

    add FAQ page

---
+++ src/pages/FaqPage/FaqPage.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Row, Col, Typography, Select, Collapse } from 'antd';
+
+import faqBg from '../../assets/faqBg.png';
+// import aboutUs1 from '../../assets/aboutUs1.png';
+import home3 from '../../assets/home3.png';
+const { Option } = Select;
+const { Paragraph } = Typography;
+const { Panel } = Collapse;
+const text = `
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s , when an unknown printer took a galley of type and scrambled it to make a type 
+`;
+
+function FaqPage() {
+  return (
+    <div>
+      <Row style={{ marginTop: '100px' }}>
+        <Col span={10} style={{ marginTop: '120px', marginLeft: '30px' }}>
+          <Row justify={'start'}>
+            {' '}
+            <Typography.Title
+              level={1}
+              style={{
+                margin: 0,
+                marginBottom: '30px',
+              }}>
+              FAQ
+            </Typography.Title>
+          </Row>{' '}
+          <Collapse>
+            <Panel
+              style={{ backgroundColor: 'white' }}
+              header='What is INVASSO ?'
+              key='1'>
+              <p style={{ color: '#5A7184', textAlign: 'left' }}>{text}</p>
+            </Panel>
+            <Panel
+              style={{ backgroundColor: 'white' }}
+              header='What is INVASSO ?'
+              key='2'>
+              <p style={{ color: '#5A7184', textAlign: 'left' }}>{text}</p>
+            </Panel>
+            <Panel
+              style={{ backgroundColor: 'white' }}
+              header='What is INVASSO ?'
+              key='3'>
+              <p style={{ color: '#5A7184', textAlign: 'left' }}>{text}</p>
+            </Panel>
+          </Collapse>
+        </Col>
+        <Col span={12}>
+          <img src={faqBg} alt='faqBg' />
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+export default FaqPage;
